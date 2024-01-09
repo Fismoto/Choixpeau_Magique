e773a87584f86fa81d212687fb24e87bc0053015
Fismoto <147399094+Fismoto@users.noreply.github.com>
2024-01-09 14:27:27 +0100

    f

---
+++ kPPV_MONTAGNE_LEFEVRE_FORTUN.py
@@ -0,0 +1,7 @@
+def distance(table, character):
+    '''
+    Cette fonction renvoie la table avec la clef 'Distance'
+    ajoutée à chaque dictionnaire.
+    La valeur de cette clef est la distance entre le character (personnage type)
+    et le personnage symbolisé par le dictionnaire.
+    '''
